76bd3bf2e3d2168f89cd02694cbe1a703a461acf
root <root@MSI.localdomain>
2020-05-10 13:43:24 +0100

    Added all needed building blocks for model training. No tests for now.

---
+++ backend/data_utils.py
@@ -0,0 +1,25 @@
+def generate_virtual_workers(n, hook):
+  workers = []
+  for i in range(n):
+    workers.append(sy.VirtualWorker(hook, id=("hospital" + str(i + 1))))
+  return workers
+
+def clear_workers(workers):
+  for w in workers:
+    w.clear_objects()
+
+def split_into_lists(data, labels):
+  record_list = list()
+  result_list = list()
+  for _, row in data.iterrows():
+    record_list.append(row)
+  for row in labels:
+    converted_label = float(row)
+    result_list.append(converted_label)
+  return record_list, result_list
+
+def convert_to_tensors(data):
+  tensors = list()
+  for record in data:
+    tensors.append(torch.tensor(record))
+  return tensors
--- backend/flask_app/app.py
+++ backend/flask_app/app.py
@@ -1,17 +1,57 @@
 from os import path
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import joblib
+import time
+import numpy as np
+import matplotlib.pyplot as plt
 from flask import Flask, request, abort, Response, json, send_from_directory
+rom sklearn.model_selection import train_test_split
 from werkzeug.utils import secure_filename
+import data_utils
+import syft as sy
+from syft.frameworks.torch.fl import utils
+hook = sy.TorchHook(torch)
 
 app = Flask(__name__)
 
 ALLOWED_FILE = 'net.py'
 SAVED_MODEL_NAME = 'model.pt'
 
+df = joblib.load("Data.bin")
+epochs = 10
+batch_size = 8
+learning_rate = 1e-2
+loss_criterion = torch.nn.BCELoss()
+number_of_workers = 3
+
 def init_syft_workers():
+    # create 3 workers and split the data between them
+    # aggregator for now just: hosts the data, tests the model
+    # for future discussion: MPC, Rewards, Crypto provider
+    workers = generate_virtual_workers(number_of_workers, hook)
+    central_server = sy.VirtualWorker(hook, id="aggregator")
+
+    # Use sklearn to split into train and test
+    X_train, X_val, y_train, y_val = train_test_split(df.drop(["ICU"], 1),
+     df["ICU"], test_size=0.2, random_state=101, stratify=df["ICU"])
+
+    # Create a federated dataset using BaseDataset for all train
+    # frames and randomly share them in an IID manner between clients
+    record_list, result_list = split_into_lists(X_train, y_train)
+    record_list = convert_to_tensors(record_list)
+    base_federated_set = sy.BaseDataset(record_list, result_list).federate(workers)
+    federated_train_loader = sy.FederatedDataLoader(base_federated_set)
+
+    test_list, test_labels = split_into_lists(X_val, y_val)
+    test_list = convert_to_tensors(test_list)
+    test_dataset = sy.BaseDataset(test_list, test_labels)
+    test_loader = torch.utils.data.DataLoader(test_dataset)
     # TODO: Implement, make necessary imports and
     # update requirements.txt file!
-    pass
+    return workers, federated_train_loader, test_loader
 
 def init_blockchain():
     # TODO: Implement, make necessary imports and
@@ -26,7 +66,31 @@ def load_model(filename):
     model = Net()
     return model
 
+# Utilize the dataset of pointers to clients data
+# to send each of them the model and get it back at the end
+# TODO: someone needs to verify this works as intended
+def train(model, federated_train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(federated_train_loader):
+        model.send(data.location)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = loss_criterion(output, target.float())
+        loss.backward()
+        optimizer.step()
+        model.get()
+        if batch_idx % 20 == 0:
+            loss = loss.get()
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * batch_size, len(federated_train_loader) * batch_size,
+                100. * batch_idx / len(federated_train_loader), loss.item()))
+
 def train_model(model):
+    optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+    %%time
+    # init hyperparameters
+    for epoch in range(1, epochs + 1):
+        train(model, federated_train_loader, optimizer, epoch)
     # TODO: Dima -> Implement federated training
     # TODO: Harry -> Perform blockchain operations
     # This function should return 2 things:
@@ -47,18 +111,18 @@ def upload_file():
         print('No file part in request!')
         abort(500)
     file = request.files['model_file']
-    
+
     if file.filename == '':
         print("No file uploaded!")
         abort(500)
-    
+
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         file.save(filename)
     else:
         print("Invalid filename. File must be named net.py")
         abort(500)
-    
+
     model = load_model(filename)
     fl_model, metrics = train_model(model)
     torch.save(fl_model.state_dict(), SAVED_MODEL_NAME)
@@ -68,7 +132,7 @@ def upload_file():
                         mimetype='application/json')
 
 
-    # TODO: Remove this when actual code is implemented. 
+    # TODO: Remove this when actual code is implemented.
     # Just here to show the front-end works as intended
     import time
     time.sleep(10)
@@ -81,11 +145,11 @@ def download_file():
         return send_from_directory(
             "", SAVED_MODEL_NAME, as_attachment=True
         )
-    
+
     print("Model has not been trained yet")
     abort(500)
 
 # TODO: Dima / Harry - Initialise Syft Workers and blockchain stuff
 # Place server initialisation code here
-init_syft_workers()
+workers, federated_train_loader, test_loader = init_syft_workers()
 init_blockchain()
