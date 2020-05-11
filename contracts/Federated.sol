58364e9fd32b4401da66c4d579814a47259219e0
Dale Cooper <dalecooper@fbi.gov>
2020-05-11 19:18:57 +0100

    add: toy contract to test integration

---
+++ contracts/Federated.sol
@@ -0,0 +1,18 @@
+pragma solidity >=0.4.21 <0.7.0;
+
+contract Federated {
+
+  mapping (address => string) models;
+  address[] public dataScientists;
+
+  function request_training(string memory _modelHash) public payable {
+    require(msg.value > 0, "You must send some ether!");
+
+    address dataScientist = msg.sender;
+
+    models[dataScientist] = _modelHash;
+    dataScientists.push(dataScientist)-1;
+  }
+
+}
+
